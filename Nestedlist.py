2e188e84d9748c31ca0c3c3a6fb9c0f656ab68ea
Devendra <devendramallarapu8@gmail.com>
2025-02-06 21:21:50 +0530

    Create Nestedlist.py

---
+++ Nestedlist.py
@@ -0,0 +1,14 @@
+#nestedList
+dict = {
+    "Personal_details":{
+        'name':"Devendra",
+        'phone number':1234567890,
+        'address':'Khammam',
+    },
+    "College_details" :{
+        'College_name':"Gcet",
+        'Bramch':"CSE-A",
+        'Year':3
+    }
+}
+print(dict)
